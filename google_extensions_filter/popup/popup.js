c2fc571b6896bd1f9321ee31a1fc9eb97c41f792
Kaleid Scoper <68382243+KaleidScoper@users.noreply.github.com>
2024-06-18 23:44:23 +0800

    浏览器扩展（版本2）

---
+++ google_extensions_filter/popup/popup.js
@@ -0,0 +1,20 @@
+// 检测（预期功能:提交数据给服务器,接受返回的值
+document.getElementById('Detect').addEventListener('click', function () {
+    chrome.tabs.executeScript({
+      code: 'window.alert("此站点为钓鱼网站！")'
+    });
+  });
+  
+  // 举报（跳转网信办
+  document.getElementById('Report').addEventListener('click', function () {
+    chrome.tabs.executeScript({
+      code: 'window.location.href = "https://www.12377.cn/wxxx/list1.html"'
+    });
+  });
+
+  // 退出（关闭网页
+  document.getElementById('Quit').addEventListener('click', function () {
+    chrome.tabs.executeScript({
+      code: 'window.location.href = "https://www.baidu.com"'
+    });
+  });
